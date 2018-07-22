d441889d5fe6fb3c78483d046bb375e26b243ca4
Adam Cox <adam_mcox@yahoo.com>
2018-07-22 14:00:09 -0400

    index layout, added pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-// JS
+// Select random "Difficulty" to begin your adventure.
+function randomNumber() {
+    var x = Math.floor((Math.random() * 9) + 1);
+    document.getElementById("difficulty").innerHTML = x;
+}
+
+// Select your character
+function randomCharacter() {
+	var myrandom=Math.round(Math.random() * 1)
+	var link1 = "zeke0.html"
+	var link2 = "julie0.html"
+
+	if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+}
+
+// Zeke's adventure
+
+// Julie's adventure
+
+// Death screen
