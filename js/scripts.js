c5f497e3ea1b2845d5a73639795aa92464e7516a
Adam Cox <adam_mcox@yahoo.com>
2018-07-26 13:25:47 -0400

    set up Zeke paths, added Julie gif files

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function randomNumber() {
     document.getElementById("difficulty").innerHTML = x;
 
 }
-
+// Button changes to "Uh-oh!".
 function mUp(obj) {
     obj.innerHTML="Uh-Oh!"; 
 }
@@ -22,6 +22,57 @@ function randomCharacter() {
 }
 
 // Zeke's adventure
+function randomZeke1() {
+    var myrandom=Math.round(Math.random() * 1)
+    var link1 = "zeke1.html"
+    var link2 = "zeke2.html"
+
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+}
+
+function randomZeke2() {
+    var myrandom=Math.round(Math.random() * 2)
+    var link1 = "zeke3.html"
+    var link2 = "zeke4.html"
+    var link3 = "zeke_dead.html"
+
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+    else if (myrandom==1)
+        window.location=link3
+}
+
+function randomZeke3() {
+    var myrandom=Math.round(Math.random() * 2)
+    var link1 = "zeke5.html"
+    var link2 = "zeke6.html"
+    var link3 = "zeke_dead.html"
+
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+    else if (myrandom==1)
+        window.location=link3
+}
+
+function randomZeke4() {
+    var myrandom=Math.round(Math.random() * 1)
+    var link1 = "zeke_end.html"
+    var link2 = "zeke_dead.html"
+
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+}
+
+
 
 // Julie's adventure
 
